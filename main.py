1d4f30b0a3be26e2867e85124f8888811429d2e7
Boominathan <eboominathan@gmail.com>
2024-03-26 06:13:44 +0530

    CRUD operation completed

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
-from fastapi import FastAPI
-from typing import Optional
-
+from uuid import uuid4, UUID
+from fastapi import FastAPI, HTTPException
+from typing import List 
+from models import Gender, Role, User, UserUpdateRequest
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-        return {"msg":"Hello Boomi"}
-    
-@app.get("/about")
-def about():
-        return {"msg":"Hello about"}
-    
-@app.get("/user/{id}")
-def user(id:int,limit:Optional[int]=None):
-    if limit is None:    
-         return {"data":id}
-    else:
-         return {"data":{"id":id,"limit":limit}}
+db: List[User] = [
+    User(
+        id=str(uuid4()),
+        first_name="Boomi",
+        last_name="Nathan",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    ),
+    User(
+        id=str(uuid4()),
+        first_name="Akshaya",
+        last_name="Kavin",
+        gender=Gender.male,
+        roles=[Role.student]
+    )
+]
+
+@app.get("/api/users")
+def users():
+    return db
+
+@app.post("/api/users")
+def createUser(user: User):
+    user.id = str(uuid4())  # Generate new UUID for the user
+    db.append(user)
+    return {"id": user.id}
+
+@app.put("/api/users/{user_id}")
+def updateUser(user_id: UUID, user_update: UserUpdateRequest):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return {"message": "User updated successfully"}
+    raise HTTPException(status_code=404, detail=f"User with id {user_id} does not exist")
+
+@app.delete("/api/users/{user_id}")
+def deleteUser(user_id:UUID):
+        for user in db:
+           if user.id == user_id:
+                db.remove(user)
+                return {"msg":"Deleted"}
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} does not exist")
